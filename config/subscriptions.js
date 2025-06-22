5f9c17f19119befad00522792b656142afb02916
hanzohanic <master_e0r1@protonmail.com>
2025-06-22 19:13:34 +0200

    update-system2

--- config/subscriptions.js
+++ config/subscriptions.js
@@ -1,1 +1,32 @@
- 
+const subscriptionTiers = {
+  starter: {
+    name: 'Starter',
+    price: 15,
+    maxDeliveries: 20,
+    maxSms: 40,
+    features: ['Basic dashboard', 'SMS alerts'],
+  },
+  basic: {
+    name: 'Basic',
+    price: 35,
+    maxDeliveries: 75,
+    maxSms: 120,
+    features: ['Delivery history'],
+  },
+  pro: {
+    name: 'Pro',
+    price: 75,
+    maxDeliveries: 200,
+    maxSms: 300,
+    features: ['Enhanced reports', 'filters'],
+  },
+  enterprise: {
+    name: 'Enterprise',
+    price: 'Custom',
+    maxDeliveries: Infinity,
+    maxSms: Infinity,
+    features: ['Custom branding', 'API access'],
+  },
+};
+
+export { subscriptionTiers }; 
